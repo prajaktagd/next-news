7a6e9653c6744eafb157dfb9e5ecab66c3903820
Prajakta Dhage <99481665+prajaktagd@users.noreply.github.com>
2024-06-24 14:31:39 +0530

    Add not found pages

--- app/news/[news_slug]/page.tsx
+++ app/news/[news_slug]/page.tsx
@@ -1,6 +1,7 @@
 "use client";
 
 import { NEWS } from "@/data/news";
+import { notFound } from "next/navigation";
 
 const NewsDetailsPage = ({
   params,
@@ -8,17 +9,19 @@ const NewsDetailsPage = ({
   const newsSlug = params.news_slug;
   const newsItem = NEWS.find((newsItem) => newsItem.slug === newsSlug);
 
+  if (!newsItem) {
+    notFound();
+  }
+
   return (
-    newsItem && (
-      <article className="news-article">
-        <header>
-          <img src={`/images/news/${newsItem.image}`} alt={newsItem.title} />
-          <h1>{newsItem.title}</h1>
-          <time dateTime={newsItem.date}>{newsItem.date}</time>
-        </header>
-        <p>{newsItem.content}</p>
-      </article>
-    )
+    <article className="news-article">
+      <header>
+        <img src={`/images/news/${newsItem.image}`} alt={newsItem.title} />
+        <h1>{newsItem.title}</h1>
+        <time dateTime={newsItem.date}>{newsItem.date}</time>
+      </header>
+      <p>{newsItem.content}</p>
+    </article>
   );
 };
 
---
+++ app/not-found.tsx
@@ -0,0 +1,10 @@
+const NotFoundPage = () => {
+  return (
+    <div id="error">
+      <h1>404 Not Found!</h1>
+      <p>The requested source could not be found.</p>
+    </div>
+  );
+};
+
+export default NotFoundPage;
