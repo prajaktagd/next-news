6d6ad96dc6c7301b5afff6a06557543415617e9a
Prajakta Dhage <99481665+prajaktagd@users.noreply.github.com>
2024-06-26 10:41:29 +0530

    Fetch the news from the newly added backend's api

--- app/(content)/news/page.tsx
+++ app/(content)/news/page.tsx
@@ -1,11 +1,37 @@
+"use client";
+
 import NewsList from "@/components/NewsList";
-import { getAllNews } from "@/lib/news";
+import { useEffect, useState } from "react";
 
 const NewsPage = () => {
+  const [isLoading, setIsLoading] = useState(false);
+  const [news, setNews] = useState([]);
+  const [error, setError] = useState("");
+
+  useEffect(() => {
+    setIsLoading(true);
+
+    fetch("http://localhost:8080/news")
+      .then(async (response) => {
+        setIsLoading(false);
+        setNews(await response.json());
+      })
+      .catch((error) => {
+        setIsLoading(false);
+        setError(error);
+      });
+  }, []);
+
   return (
     <>
       <h1>Headlines</h1>
-      <NewsList news={getAllNews()} />
+      {isLoading ? (
+        <p>Loading...</p>
+      ) : error ? (
+        <p>Failed to fetch the news.</p>
+      ) : (
+        <NewsList news={news} />
+      )}
     </>
   );
 };
